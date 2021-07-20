e46f09b1271f5f15be368806c316791afcd7a8f1
Adam Saulters <adamsaulters@gmail.com>
2021-07-20 16:34:31 -0400

    books create/index/new

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Book = ('../models/book')
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/", async (req, res) => {
+  let books;
+  try{
+    books = await find().sort({createAt: 'desc'}).limit(10).exec();
+  } catch {
+    books = []
+  }
+  res.render("index", {books: books});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-// if (process.env.NODE_ENV !== 'production') {
-//   require('dotenv').load()
-// }
 
 const express = require('express')
 const app = express()
